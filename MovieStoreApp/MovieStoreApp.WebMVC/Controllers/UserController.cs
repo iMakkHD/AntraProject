cf961c1b36d2b108436fdc503080930c8e977206
Steven Mak <maksteven2022@gmail.com>
2022-04-14 05:47:45 -0500

    All empty homework 1 files

---
+++ MovieStoreApp/MovieStoreApp.WebMVC/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieStoreApp.WebMVC.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
