0e4f3b888aae57643471680e2a0131d5c950e677
Steven Mak <maksteven2022@gmail.com>
2022-04-14 10:04:54 -0500

    All class created. Entity fields may not have correct types. Movie does not have all fields as instructed by instructor.

---
+++ MovieStoreApp/MovieStoreApp.Infrastructure/Entity/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieStoreApp.Infrastructure.Entity
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public int LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string HashedPassword { get; set; }
+        public string Salt { get; set; }
+        public string PhoneNumber { get; set; }
+        public Boolean TwoFactorEnabled { get; set; }
+        public DateTime LockedEndDate { get; set; }
+        public DateTime LastLoginDateTime { get; set; }
+        public Boolean IsLocked { get; set; }
+        public int AccessFailedCount { get; set; }
+
+    }
+}
